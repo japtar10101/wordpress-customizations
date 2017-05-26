d07b566fac7420cb5b81549472a3d3a7658f92fc
Taro Omiya <taro.omiya@omiyagames.com>
2017-05-26 16:03:01 -0400

    Adding pagination to blog site.

--- Themes/twentyseventeen-taroomiya/home.php
+++ Themes/twentyseventeen-taroomiya/home.php
@@ -28,6 +28,11 @@ get_header(); ?>
 				while ( have_posts() ) : the_post();
 					get_template_part( 'template-parts/post/content', 'excerpt' );
 				endwhile;
+				the_posts_pagination( array(
+					'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
+					'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
+					'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
+				) );
 			endif; ?>
 
 		</main><!-- #main -->
