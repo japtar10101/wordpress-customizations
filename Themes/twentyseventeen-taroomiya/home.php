59b8fd188c7683aa964af4c519f09fc9cc22afa0
Taro Omiya <japtar10101@gmail.com>
2017-05-26 15:51:01 -0400

    Working on updating the blog page.

---
+++ Themes/twentyseventeen-taroomiya/home.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The blog page template file
+ *
+ * If the user has selected a static page for their homepage, this is what will
+ * appear.
+ * Learn more: https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 0.1
+ * @version 0.1
+ */
+
+get_header(); ?>
+
+<div class="wrap">
+	<header class="page-header">
+		<h1 class="page-title">Blog</h1>
+	</header><!-- .page-header -->
+		
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php // Show the selected frontpage content.
+			if ( have_posts() ) :
+				while ( have_posts() ) : the_post();
+					get_template_part( 'template-parts/post/content', 'excerpt' );
+				endwhile;
+			endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div>
+<?php get_footer();
