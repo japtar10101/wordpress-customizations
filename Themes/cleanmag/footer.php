75d60a94c6d1690072e34eb5e099ac97f67aeca6
Taro Omiya <taro.omiya@omiyagames.com>
2017-08-03 00:40:49 -0400

    Attempting to replace all instance of http to https.

--- Themes/cleanmag/footer.php
+++ Themes/cleanmag/footer.php
@@ -13,7 +13,7 @@ $options = get_option( 'cleanmag_theme_settings' );
     <div id="copyright" class="container_24">
     <?php
     ?>
-    &copy; <?php echo date('Y'); ?>  <?php bloginfo( 'name' ) ?> ~ <a href="http://omiyagames.com/privacy-policy" title="Privacy Policy" rel="nofollow">Privacy Policy</a>
+    &copy; <?php echo date('Y'); ?>  <?php bloginfo( 'name' ) ?> ~ <a href="https://omiyagames.com/privacy-policy" title="Privacy Policy" rel="nofollow">Privacy Policy</a>
     </div>
 </div>
 <?php wp_footer(); ?>
